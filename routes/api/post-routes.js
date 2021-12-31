ef76be9b188ab086d201ddd23536a860268960a0
burcar <zacharyburcar@gmail.com>
2021-12-31 16:57:15 -0500

    couple bug fixes

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,16 +1,23 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const { Post, User, Vote } = require('../../models');
+const sequelize = require('../../config/connection');
 
 // get all posts
 router.get('/', (req, res) => {
     console.log('========================');
     Post.findAll({
-        attributes: ['id', 'post_url', 'title', 'created_at'],
+        attributes: [
+        'id', 
+        'post_url',
+        'title',
+        'created_at',
+        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
         order: [['created_at', 'DESC']],
         include: [
             {
-                model: User,
-                attributes: ['username']
+            model: User,
+            attributes: ['username']
             }
         ]
     })
@@ -27,16 +34,25 @@ router.get('/:id', (req, res) => {
         where: {
             id: req.params.id
         },
-        attributes: ['id', 'post_url', 'title', 'created_at'],
+        attributes: [
+        'id',
+        'post_url',
+        'title',
+        'created_at',
+        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
         include: [
             {
-                model: User,
-                attributes: ['username']
+            model: User,
+            attributes: ['username']
             }
         ]
     })
     .then(dbPostData => {
         if (!dbPostData) {
+
+
+
             res.status(404).json({ message: 'No post found with this id' });
             return;
         }
@@ -61,6 +77,38 @@ router.post('/', (req, res) => {
             res.status(500).json(err);
         });
 });
+
+// PUT api/posts/upvote
+// this is declared before :id PUT route as to avoid express thinking /upvote is valid :id parameter
+router.put('/upvote', (req, res) => {
+    Vote.create({
+        user_id: req.body.user_id,
+        post_id: req.body.post_id
+    }).then(() => {
+        // then find the post we just voted on
+        return Post.findOne({
+            where: {
+                id: req.body.post_id
+            },
+            attributes: [
+                'id',
+                'post_url',
+                'title',
+                'created_at',
+                // use raw MySql aggregate function query to get a count of how many votes a post has and return it under the name vote_count
+                [
+                    sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
+                    'vote_count'
+                ]
+            ]
+        })
+        .then(dbPostData => res.json(dbPostData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    })
+})
 
 router.put('/:id', (req, res) => {
     Post.update(
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User, Post, Vote } = require('../../models');
 
 // GET /api/users
 router.get('/', (req, res) => {
@@ -17,10 +17,22 @@ router.get('/', (req, res) => {
 // GET /api/users/#
 router.get('/:id', (req, res) => {
     User.findOne({
-        attributes: { exclude: ['password'] },
         where: {
             id: req.params.id
-        }
+        },
+        attributes: { exclude: ['password'] },
+        include: [
+            {
+                model: Post,
+                attributes: ['id', 'title', 'post_url', 'created_at']
+            },
+            {
+                model: Post,
+                attributes: ['title'],
+                through: Vote,
+                as: 'voted_posts'
+            }
+        ]
     })
         .then(dbUserData => {
             if (!dbUserData) {
@@ -63,7 +75,7 @@ router.post('/login', (req, res) => {
         }
 
         const validPassword = dbUserData.checkPassword(req.body.password);
-        if (!validPassword) {
+        if (!validassword) {
             res.status(400).json({ message: 'Incorrect password!' });
             return;
         }
